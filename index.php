8a2fb27c639b615872dcac3a4c67b4b436139cb7
Char Mike <storagewars999@gmail.com>
2020-12-04 10:57:47 +0200

    alkuvaiheita

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Etusivu</title>
+</head>
+<body>
+    
+    <form action="">
+    <label for="sender">Nimesi:</label>
+  <input type="text" id="sender" name="sender"><br><br>
+  <label for="msg">Viestisi:</label>
+  <input type="text" id="msg" name="msg"><br><br>
+  <input type="submit" value="Lähetä!">
+    </form>
+</body>
+</html>
